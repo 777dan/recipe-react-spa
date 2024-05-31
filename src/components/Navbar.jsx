8f74b2713fd9811187661f2fa81885b0507f0d26
Danoslo 255 <danya.nikitin7@gmail.com>
2024-05-31 14:09:06 +0300

    changed routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,10 +22,10 @@ const Navbar = ({setSearchResult}) => {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/recipe-react-spa/#/">Home</a>
+                <a className="nav-link active" aria-current="page" href="/">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/recipe-react-spa/#/about">About</a>
+                <a className="nav-link active" aria-current="page" href="/about">About</a>
               </li>
             </ul>
             <form className="d-flex" onSubmit={handleSearch}>
